eda44578bfe1abf5802df9ada8f1e302eb3fdb5f
Carlos Guerrero <guerrerocarlos@gmail.com>
2014-07-16 20:30:53 -0400

    Folders Reorganization

---
+++ Gruntfile.js
@@ -0,0 +1,166 @@
+
+var parseBuildPlatforms = function(argumentPlatform) {
+	// this will make it build no platform when the platform option is specified
+	// without a value which makes argumentPlatform into a boolean
+	var inputPlatforms = argumentPlatform || process.platform + ";" + process.arch;
+
+	// Do some scrubbing to make it easier to match in the regexes bellow
+	inputPlatforms = inputPlatforms.replace("darwin", "mac");
+	inputPlatforms = inputPlatforms.replace(/;ia|;x|;arm/, "");
+
+	var buildAll = /^all$/.test(inputPlatforms);
+
+	var buildPlatforms = {
+		mac: /mac/.test(inputPlatforms) || buildAll,
+		win: /win/.test(inputPlatforms) || buildAll,
+		linux32: /linux32/.test(inputPlatforms) || buildAll,
+		linux64: /linux64/.test(inputPlatforms) || buildAll
+	};
+
+	return buildPlatforms;
+};
+
+module.exports = function(grunt) {
+	"use strict";
+
+	var buildPlatforms = parseBuildPlatforms(grunt.option('platforms'));
+	var currentVersion = grunt.file.readJSON('package.json').version;
+
+	require('load-grunt-tasks')(grunt);
+
+	grunt.registerTask('build', [
+		'nodewebkit'
+	]);
+
+	grunt.registerTask('dist', [
+		'clean:releases',
+		'build',
+		'exec:createDmg', // mac
+		'exec:createWinInstall',
+		'compress' // win & linux
+	]);	
+
+	grunt.registerTask('start', function(){
+		var start = parseBuildPlatforms();
+		if(start.win){
+			grunt.task.run('exec:win');
+		}else if(start.mac){
+			grunt.task.run('exec:mac');
+		}else if(start.linux32){
+			grunt.task.run('exec:linux32');
+		}else if(start.linux64){
+			grunt.task.run('exec:linux64');
+		}else{
+			grunt.log.writeln('OS not supported.');
+		}
+	});
+
+		nodewebkit: {
+			options: {
+				version: '0.9.2',
+				build_dir: './build', // Where the build version of my node-webkit app is saved
+				keep_nw: true,
+				embed_nw: false,
+				mac_icns: './src/app/images/nw.icns', // Path to the Mac icon file
+				zip: buildPlatforms.win, // Zip nw for mac in windows. Prevent path too long if build all is used.
+				mac: buildPlatforms.mac,
+				win: buildPlatforms.win,
+				linux32: buildPlatforms.linux32,
+				linux64: buildPlatforms.linux64,
+				download_url: 'http://cdn.popcorntime.io/nw/'
+			},
+			src: ['./src/**', '!./src/app/styl/**', 
+				'./node_modules/**', '!./node_modules/bower/**', '!./node_modules/*grunt*/**', '!./node_modules/stylus/**', 
+				'!./**/test*/**', '!./**/doc*/**', '!./**/example*/**', '!./**/demo*/**', '!./**/bin/**', '!./**/build/**', '!./**/.*/**', 
+				'./package.json', './README.md', './LICENSE.txt' ]
+		},
+		
+		exec: {
+			win: {
+				cmd: '"build/cache/win/<%= nodewebkit.options.version %>/nw.exe" .'
+			},
+			mac: {
+				cmd: 'build/cache/mac/<%= nodewebkit.options.version %>/node-webkit.app/Contents/MacOS/node-webkit .'
+			},
+			linux32: {
+				cmd: '"build/cache/linux32/<%= nodewebkit.options.version %>/nw" .'
+			},
+			linux64: {
+				cmd: '"build/cache/linux64/<%= nodewebkit.options.version %>/nw" .'
+			},
+			createDmg: {
+				cmd: 'dist/mac/yoursway-create-dmg/create-dmg --volname "TorrenTV' + currentVersion + '" --background ./dist/mac/background.png --window-size 480 540 --icon-size 128 --app-drop-link 240 370 --icon "TorrenTV" 240 110 ./build/releases/TorrenTV/mac/TorrenTV-' + currentVersion + '-Mac.dmg ./build/releases/TorrenTV/mac/'
+			},
+			createWinInstall: {
+				cmd: 'makensis dist/windows/installer.nsi'
+			}
+		},
+
+		jshint: {
+			gruntfile: {
+				options: {
+					jshintrc: '.jshintrc'
+				},
+				src: 'Gruntfile.js'
+			},
+			src: {
+				options: {
+					jshintrc: 'src/app/.jshintrc'
+				},
+				src: ['src/app/*.js']
+			}
+		},
+
+		compress: {
+			linux32: {
+				options: {
+					mode: 'tgz',
+					archive: 'build/releases/Popcorn-Time/linux32/TorrenTV-' + currentVersion + '-Linux-32.tar.gz'
+				},
+				expand: true,
+				cwd: 'build/releases/TorrenTV/linux32/TorrenTV',
+				src: '**',
+				dest: 'TorrenTV'
+			},
+			linux64: {
+				options: {
+					mode: 'tgz',
+					archive: 'build/releases/TorrenTV/linux64/TorrenTV-' + currentVersion + '-Linux-64.tar.gz'
+				},
+				expand: true,
+				cwd: 'build/releases/TorrenTV/linux64/TorrenTV',
+				src: '**',
+				dest: 'TorrenTV'
+			},
+			windows: {
+				options: {
+					mode: 'zip',
+					archive: 'build/releases/TorrenTV/win/TorrenTV-' + currentVersion + '-Win.zip'
+				},
+				expand: true,
+				cwd: 'dist/windows',
+				src: 'TorrenTV.exe',
+				dest: ''
+			}			
+		},
+
+		clean: {
+			releases: ['build/releases/TorrenTV/**']
+		},
+		
+		watch: {
+			options: {
+				dateFormat: function(time) {
+					grunt.log.writeln('Completed in ' + time + 'ms at ' + (new Date()).toLocaleTimeString());
+					grunt.log.writeln('Waiting for more changes...');
+				}
+			},
+			scripts: {
+				files: ['./src/app/styl/*.styl','./src/app/styl/**/*.styl'],
+				tasks: ['css']
+			}
+		}
+
+	});
+
+};
---
+++ dist/windows/installer.nsi
@@ -0,0 +1,124 @@
+; TorrenTV 
+; Installer Source
+; Version 1.0
+
+;Include Modern UI
+!include "MUI2.nsh"
+
+;General Settings
+!searchparse /file "../../package.json" `  "version": "` PT_VERSION `",`
+!searchreplace PT_VERSION "${PT_VERSION}" "-" "."
+Name "TorrenTV"
+Caption "TorrenTV v${PT_VERSION}"
+BrandingText "TorrenTV v${PT_VERSION}"
+VIAddVersionKey "ProductName" "TorrenTV"
+VIAddVersionKey "ProductVersion" "v${PT_VERSION}"
+VIAddVersionKey "FileDescription" "TorrenTV v${PT_VERSION} Installer"
+VIAddVersionKey "FileVersion" "v${PT_VERSION}"
+VIAddVersionKey "CompanyName" "Popcorn Official"
+VIAddVersionKey "LegalCopyright" "http://popcorntime.io"
+VIProductVersion "${PT_VERSION}.0"
+OutFile "PopcornTimeSetup.exe"
+CRCCheck on
+SetCompressor /SOLID lzma
+
+;Default installation folder
+InstallDir "$LOCALAPPDATA\TorrenTV"
+
+;Request application privileges
+RequestExecutionLevel user
+
+;Define UI settings
+!define MUI_LICENSEPAGE_BGCOLOR /GRAY
+!define MUI_UI_HEADERIMAGE_RIGHT "..\..\src\app\images\icon.png"
+!define MUI_ICON "..\..\src\app\images\popcorntime.ico"
+!define MUI_UNICON "..\..\src\app\images\popcorntime.ico"
+!define MUI_WELCOMEFINISHPAGE_BITMAP "installer-image.bmp"
+!define MUI_UNWELCOMEFINISHPAGE_BITMAP "uninstaller-image.bmp"
+!define MUI_ABORTWARNING
+!define MUI_FINISHPAGE_LINK "TorrenTV Official Homepage"
+!define MUI_FINISHPAGE_LINK_LOCATION "http://popcorntime.io/"
+
+;Define the pages
+!insertmacro MUI_PAGE_LICENSE "LICENSE.txt"
+!insertmacro MUI_PAGE_INSTFILES
+!insertmacro MUI_PAGE_FINISH
+
+;Define uninstall pages
+!insertmacro MUI_UNPAGE_CONFIRM
+!insertmacro MUI_UNPAGE_INSTFILES
+!insertmacro MUI_UNPAGE_FINISH
+
+;Load Language Files
+!insertmacro MUI_LANGUAGE "English"
+!insertmacro MUI_LANGUAGE "Spanish"
+!insertmacro MUI_LANGUAGE "SpanishInternational"
+
+Section ; Node Webkit Files
+
+	;Delete existing install
+	RMDir /r "$INSTDIR"
+
+	;Set output path to InstallDir
+	SetOutPath "$INSTDIR\node-webkit"
+
+	;Add the files
+	File "..\..\build\cache\win\0.9.2\*.dll"
+	File "/oname=TorrenTV.exe" "..\..\build\cache\win\0.9.2\nw.exe"
+	File "..\..\build\cache\win\0.9.2\nw.pak"
+
+SectionEnd
+
+Section ; App Files
+
+	;Set output path to InstallDir
+	SetOutPath "$INSTDIR\src\app"
+
+	;Add the files
+	File /r "..\..\src\app\css"
+	File /r "..\..\src\app\fonts"
+	File /r "..\..\src\app\images"
+	File /r "..\..\src\app\language"
+	File /r "..\..\src\app\lib"
+	File /r "..\..\src\app\templates"
+	File /r "..\..\src\app\vendor"
+	File "..\..\src\app\index.html"
+	File "..\..\src\app\*.js"
+
+	SetOutPath "$INSTDIR"
+	File "..\..\package.json"
+
+	SetOutPath "$INSTDIR\node_modules"
+	File /r /x "*grunt*" /x "stylus" /x "bower" /x ".bin" /x "bin" /x "test"  /x "test*" /x "example*" "..\..\node_modules\*.*"
+
+	;Create uninstaller
+	WriteUninstaller "$INSTDIR\Uninstall.exe"
+
+SectionEnd
+
+Section ; Shortcuts
+
+	;Working Directory
+	SetOutPath "$INSTDIR"
+    
+	CreateShortCut "$INSTDIR\TorrenTV.lnk" "$INSTDIR\node-webkit\TorrenTV.exe" "." "$INSTDIR\src\app\images\popcorntime.ico" "" "" "" "TorrenTV"
+
+	;Start Menu Shortcut
+	RMDir /r "$SMPROGRAMS\TorrenTV"
+	CreateDirectory "$SMPROGRAMS\TorrenTV"
+	CreateShortCut "$SMPROGRAMS\TorrenTV\TorrenTV.lnk" "$INSTDIR\node-webkit\TorrenTV.exe" "." "$INSTDIR\src\app\images\popcorntime.ico" "" "" "" "TorrenTV"
+	CreateShortCut "$SMPROGRAMS\TorrenTV\Uninstall.lnk" "$INSTDIR\Uninstall.exe" "" "$INSTDIR\src\app\images\popcorntime.ico" "" "" "" "Uninstall TorrenTV"
+
+	;Desktop Shortcut
+	Delete "$DESKTOP\TorrenTV.lnk"
+	CreateShortCut "$DESKTOP\TorrenTV.lnk" "$INSTDIR\node-webkit\TorrenTV.exe" "." "$INSTDIR\src\app\images\popcorntime.ico" "" "" "" "TorrenTV"
+
+SectionEnd
+
+Section "uninstall" ; Uninstaller
+
+	RMDir /r "$INSTDIR"
+	RMDir /r "$SMPROGRAMS\TorrenTV"
+	Delete "$DESKTOP\TorrenTV.lnk"
+	
+SectionEnd
