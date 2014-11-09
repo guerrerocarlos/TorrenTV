3aab1c5c69088fdb22b7924fa421fff7a9370f4b
Carlos Guerrero <guerrerocarlos@gmail.com>
2014-11-09 17:13:44 -0300

    Adding missing files
    Signed-off-by: Carlos Guerrero <guerrerocarlos@gmail.com>

---
+++ src/app/subtitlesServer.js
@@ -0,0 +1,55 @@
+var server;
+var httpServer;
+var PORT = 9999;
+var subData = '';
+var http = require('http');
+var iconv = require('iconv-lite');
+
+server = http.createServer(function (req, res) {
+    if (req.headers.origin) {
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    }
+
+    res.writeHead(200, {
+        'Content-Type': 'text/vtt'
+    });
+    res.end(subData);
+});
+
+function startListening(cb) {
+    httpServer = server.listen(PORT);
+}
+
+function stopServer(cb) {
+    httpServer.close(function () {
+        if (cb) {
+            cb();
+        }
+    });
+}
+
+var SubtitlesServer = {
+    start: function (data, cb) {
+        iconv.extendNodeEncodings();
+        var vtt = data.vtt;
+        var encoding = data.encoding;
+        try {
+            fs.readFile(vtt, {}, function (err, data) {
+                subData = data;
+                if (httpServer) {
+                    stopServer(startListening(cb));
+                } else {
+                    startListening(cb);
+                }
+            });
+        } catch (e) {
+            console.log('Error Reading vtt');
+        }
+    },
+
+    stop: function () {
+        stopServer();
+    }
+};
+
+module.exports = SubtitlesServer
